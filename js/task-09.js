2d92b4afa9361cc57122245a55c98a1743999c6f
Ddemkowicz <dakucp@gmail.com>
2023-02-19 08:12:41 +0100

    up

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,10 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const body = document.querySelector("body");
+const btnChangeColor = document.querySelector(".change-color");
+const color = document.querySelector(".color");
+btnChangeColor.addEventListener("click", () => {
+  body.style.backgroundColor = getRandomHexColor();
+  color.textContent = getRandomHexColor();
+});
